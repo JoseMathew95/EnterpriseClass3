be18a9daa6310afedb5b6016d5c18f8dc09bad2e
josemathew95 <jos.india@yahoo.com>
2016-05-27 21:56:47 -0400

    added navbar user controls

---
+++ class3/User_Controls/NavBar.ascx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace class3
+{
+    public partial class NavBar : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            setActivePage();
+        }
+
+        private void setActivePage()
+        {
+            switch (Page.Title)
+            {
+                case "Home Page":
+                    home.Attributes.Add("class", "active");
+                    break;
+                case "Contacts":
+                    contacts.Attributes.Add("class", "active");
+                    break;
+            }
+        }
+    }
+}
